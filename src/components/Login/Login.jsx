afebf9af2d2141c4c0f1fb5f44040cedd8e2aee4
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-21 13:31:27 +0600

    use google sing in 1st part done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,19 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../../firebase/firebase.init';
 
 const Login = () => {
+    const provider= new GoogleAuthProvider
     const handleGoogleSingIn =()=>{
         console.log("google sing in clicked");
+
+        signInWithPopup(auth,provider)
+        .then(result =>{
+            console.log(result);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
     }
     return (
         <div>
