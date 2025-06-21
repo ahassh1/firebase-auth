b8c5cd4dffef8b3f127078b3bc5c7132f0d2cf63
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-21 23:50:41 +0600

    handle no user email due to provider duplication

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,68 +1,85 @@
-import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import React, { useState } from 'react';
-import { auth } from '../../firebase/firebase.init';
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../../firebase/firebase.init";
 
 const Login = () => {
-    const [user, setUser] = useState(null)
-    const provider = new GoogleAuthProvider()
+  const [user, setUser] = useState(null);
+  const provider = new GoogleAuthProvider();
 
-    const githubProvider = new GithubAuthProvider();
-    
-    const handleGoogleSingIn = () => {
-        console.log("google sing in clicked");
+  const githubProvider = new GithubAuthProvider();
 
-        signInWithPopup(auth, provider)
-            .then(result => {
-                console.log(result.user);
-                setUser(result.user)
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
+  const handleGoogleSingIn = () => {
+    console.log("google sing in clicked");
 
-    const handleSingOut = () => {
-        signOut(auth)
-            .then(() => {
-                console.log('sing-out completed');
-                setUser(null);
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const handleGithubSingIn =()=>{
-        signInWithPopup(auth,githubProvider)
-        .then(result=>{
-            console.log(result);
-            setUser(result.user)
-        })
-        .catch(error =>{
-            console.log(error);
-        })
-    }
+  const handleSingOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("sing-out completed");
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
+  const handleGithubSingIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        // console.log(result);
+        // setUser(result.user);
+
+                const loggedInUser = result.user;
+                console.log(loggedInUser)
+
+                if(!loggedInUser.email && loggedInUser?.providerData?.length ){
+                    console.log('user email is not directly provided')
+                    if(loggedInUser.providerData[0].email){
+                        loggedInUser.email = loggedInUser.providerData[0].email
+                        setUser(loggedInUser);
+                    }
+                }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Please login now</h2>
+      {user ? (
+        <button onClick={handleSingOut}>Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSingIn}>Sign in with Google</button>
+          <button onClick={handleGithubSingIn}>Sing in with Github</button>
+        </>
+      )}
+
+      {user && (
         <div>
-            <h2>Please login now</h2>
-            {user ? 
-                <button onClick={handleSingOut}>Sign Out</button> :
-               <>
-                <button onClick={handleGoogleSingIn}>Sign in with Google</button>
-                <button onClick={handleGithubSingIn}>Sing in with Github</button>
-               </>
-            }
-
-          {
-            user && <div>
-            <h2>{user.displayName}</h2>
-            <p>{user.email}</p>
-            <img src={user.photoURL} alt="" />
-          </div>
-          }
+          <h2>User Name:{user.displayName}</h2>
+          <p> User Email:{user.email}</p>
+          <img src={user.photoURL} alt="" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;
