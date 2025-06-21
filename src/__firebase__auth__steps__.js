12d2fece56b004c4e8fc50f3be6a560604b9d6cb
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-21 17:39:33 +0600

    toggle sing in and sing Out button based on user state

--- src/__firebase__auth__steps__.js
+++ src/__firebase__auth__steps__.js
@@ -3,4 +3,7 @@
  * 1.create a project in the firebase console 
  * 2. in the project install firebase: npm i firebase
  * 3. bring firebase config into your project (form firebase website)
+ * 4. by using useAuth create the auth and export it 
+ * 5. in the login page: create a provider new GoogleAuthProvider()
+ * 6. use singInWithPopUp and passs auth the provider
  */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,50 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
 import { auth } from '../../firebase/firebase.init';
 
 const Login = () => {
-    const provider= new GoogleAuthProvider
-    const handleGoogleSingIn =()=>{
+    const [user, setUser] = useState(null)
+    const provider = new GoogleAuthProvider()
+    
+    const handleGoogleSingIn = () => {
         console.log("google sing in clicked");
 
-        signInWithPopup(auth,provider)
-        .then(result =>{
-            console.log(result);
-        })
-        .catch(error =>{
-            console.log(error);
-        })
+        signInWithPopup(auth, provider)
+            .then(result => {
+                console.log(result.user);
+                setUser(result.user)
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
+
+    const handleSingOut = () => {
+        signOut(auth)
+            .then(() => {
+                console.log('sing-out completed');
+                setUser(null);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     return (
         <div>
             <h2>Please login now</h2>
-            <button onClick={handleGoogleSingIn}>Sing in with Google</button>
+            {user ? 
+                <button onClick={handleSingOut}>Sign Out</button> :
+                <button onClick={handleGoogleSingIn}>Sign in with Google</button>
+            }
+
+          {
+            user && <div>
+            <h2>{user.displayName}</h2>
+            <p>{user.email}</p>
+            <img src={user.photoURL} alt="" />
+          </div>
+          }
         </div>
     );
 };
